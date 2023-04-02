4a129c5efb7b5cea152c14214a7992895d6b2734
himanshukingstorm <126673931+himanshukingstorm@users.noreply.github.com>
2023-04-02 13:07:52 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello WorlD");
+		get("/hello", (req, res) -> "Hello Rahul, Nitin and Shivani");
 	}
 }
